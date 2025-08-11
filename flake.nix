7b68acc2a84c99e7991339f36c6e2b15a6314f79
Ruben Hoenle <git@hoenle.xyz>
2025-08-11 15:19:02 +0200

    feat(nix): add dev shell

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, treefmt-nix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+
+        treefmtEval = treefmt-nix.lib.evalModule pkgs {
+          projectRootFile = "flake.nix";
+          programs = {
+            nixpkgs-fmt.enable = true;
+          };
+        };
+      in
+      {
+        formatter = treefmtEval.config.build.wrapper;
+        checks.formatter = treefmtEval.config.build.check self;
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            terraform
+            go
+          ];
+        };
+      }
+    );
+}
